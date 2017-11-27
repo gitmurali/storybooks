053c185da9ef541e2db05b4e4c3f027505bdf926
murali.prashanth <murali.prashanth@specialistholidays.com>
2017-11-27 15:55:37 +0530

    adds the mongo uri, google oauth client secret keys

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://gomurali:admin@ds139342.mlab.com:39342/storybooks-bob',
+    googleClientID: '562412953327-decmu4pfqpph6vlv87eqr7cht6inmiq0.apps.googleusercontent.com',
+    googleClientSecret: 'VqRdKSdpo99VHxO02diSMu6p'
+}
