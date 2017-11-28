d323ed61792517b3caecf19816fb2ec204cef36f
murali.prashanth <murali.prashanth@specialistholidays.com>
2017-11-28 20:19:02 +0530

    Adds authenticated routes

--- routes/stories.js
+++ routes/stories.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuthenticated} = require('../helpers/auth');
 
 router.get('/', (req, res) => {
     res.render('stories/index');
 });
 
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('stories/add');
 });
 
