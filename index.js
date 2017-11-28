85fb5b98f98055f44947eda16926990a75ea3ea5
murali.prashanth <murali.prashanth@specialistholidays.com>
2017-11-28 16:08:33 +0530

    adds header, logout button..

--- index.js
+++ index.js
@@ -1,63 +1,65 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
-const auth = require('./routes/auth');
-const index = require('./routes/index');
-const passport = require('passport');
-const keys = require('./config/keys');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const passport = require('passport');
 
-//assign global promise to mongoose promise
+// Load User Model
+require('./models/user');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const index = require('./routes/index');
+const auth = require('./routes/auth');
+
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promises
 mongoose.Promise = global.Promise;
 
-//connect to mongo db
+// Mongoose Connect
 mongoose.connect(keys.mongoURI, {
-   useMongoClient: true
-}).then(() => {
-    console.log('mongodb connected!')
-}).catch(err => {
-    console.log(err);
-});
+    useMongoClient:true
+})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 const app = express();
 
-//load user model
-require('./models/user');
-
-// passport config
-require('./config/passport')(passport);
-
-//Handlebars middleware
+// Handlebars Middleware
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
+    defaultLayout:'main'
 }));
 app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
-
 app.use(session({
-    secret: 'secret cat',
+    secret: 'secret',
     resave: false,
-    saveUninitialized: false,
-    cookie: { secure: true }
+    saveUninitialized: false
 }));
 
-//passport middleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-//set global vars
+// Set global vars
 app.use((req, res, next) => {
     res.locals.user = req.user || null;
     next();
 });
 
-//Load routes
+// Use Routes
 app.use('/', index);
 app.use('/auth', auth);
 
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
-    console.log(`server started on port: ${port}`);
+    console.log(`Server started on port ${port}`)
 });
