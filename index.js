59fcd14553c0d376941013733686fabe152099ed
gitmurali <info@muraliprashanth.me>
2017-11-29 16:23:42 +0530

    save story to mongo

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
 
-// Load User Model
+// Load models
 require('./models/user');
+require('./models/story');
 
 // Passport Config
 require('./config/passport')(passport);
@@ -30,6 +32,12 @@ mongoose.connect(keys.mongoURI, {
     .catch(err => console.log(err));
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
--- routes/stories.js
+++ routes/stories.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Story = mongoose.model('stories');
+const User = mongoose.model('users');
 const {ensureAuthenticated} = require('../helpers/auth');
 
 router.get('/', (req, res) => {
@@ -10,4 +13,27 @@ router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('stories/add');
 });
 
+router.post('/', (req, res) => {
+    let allowComments;
+    if (req.body.allowComments) {
+        allowComments = true;
+    } else {
+        allowComments = false;
+    }
+
+    const newStory = {
+        title: req.body.title,
+        body: req.body.body,
+        status: req.body.status,
+        allowComments: allowComments,
+        user: req.user.id,
+    }
+
+    //create story
+    new Story(newStory)
+        .save()
+        .then(story => {
+            res.redirect(`/stories/show/${story.id}`);
+        });
+});
 module.exports = router;
