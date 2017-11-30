b9c441229d6e612b5a9efdb39fb868bab10bb8e7
gitmurali <info@muraliprashanth.me>
2017-11-30 17:32:43 +0530

    adds prod variables

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://gomurali:admin@ds139342.mlab.com:39342/storybooks-bob',
-    googleClientID: '562412953327-decmu4pfqpph6vlv87eqr7cht6inmiq0.apps.googleusercontent.com',
-    googleClientSecret: 'VqRdKSdpo99VHxO02diSMu6p'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
